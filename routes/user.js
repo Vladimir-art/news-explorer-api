bc890465048d4d3db841b4546236ba6b40f8f1e1
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-15 22:36:41 +0300

    Add user's ctrls to Route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const user = require('express').Router();
+const { getUser } = require('../controllers/user');
 
-user.get('users/me', { someCode });
+user.get('users/me', getUser);
 
 module.exports = { user };
