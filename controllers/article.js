9638218aaa16297d1b2b9a2ca4f188a4a17dfb64
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-15 22:32:16 +0300

    Delete article

--- controllers/article.js
+++ controllers/article.js
@@ -9,3 +9,41 @@ module.exports.getArticles = (req, res, next) => {
     .then((u) => res.status(200).send(u))
     .catch(next);
 };
+
+module.exports.createArticle = (req, res, next) => {
+  const {
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+  } = req.body;
+  const owner = req.body._id;
+
+  Article.create({
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+    owner,
+  })
+    .then((article) => res.status(200).send(article))
+    .catch(next);
+};
+
+module.exports.deleteArticle = (req, res, next) => {
+  Article.findByIdAndRemove(req.params.id)
+    .then((c) => {
+      if (c !== null) {
+        res.status(200).send(c);
+      } else {
+        res.status(404).send({ message: 'Данной статьи не существует' });
+      }
+    })
+    .catch(next);
+};
