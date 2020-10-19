1a2c82d50319b7b22d8151108353e53592310b25
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-19 21:53:37 +0300

    Update createArticle router

--- routes/article.js
+++ routes/article.js
@@ -9,11 +9,10 @@ article.post('/', celebrate({
     keyword: Joi.string().required(),
     title: Joi.string().required(),
     text: Joi.string().required(),
-    date: Joi.date().options({ convert: false }).required(),
+    date: Joi.string().required(),
     source: Joi.string().required(),
     link: Joi.string().uri().required(),
     image: Joi.string().uri().required(),
-    owner: Joi.string().hex().required(),
   }),
 }), createArticle);
 
