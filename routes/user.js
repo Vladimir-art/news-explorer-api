323d909cdf411df79f34227c23707ee4db5d5a8a
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-20 22:35:18 +0300

    Update getUser's Joi

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,11 @@
 const user = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getUser } = require('../controllers/user');
 
-user.get('/me', getUser);
+user.get('/me', celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+  }),
+}), getUser);
 
 module.exports = { user };
