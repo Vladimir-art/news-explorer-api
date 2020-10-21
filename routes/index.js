e20ed1d66b041b473c91d64f003379295c5dbb18
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-10-21 23:27:41 +0300

    Add commit in commonRouter

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const commonRouter = require('express').Router();
 const { article } = require('./article');
 const { user } = require('./user');
 
+// Все роуты подключены в файле index.js, который находится в папке routes.
 commonRouter.use('/users', user);
 commonRouter.use('/articles', article);
 
